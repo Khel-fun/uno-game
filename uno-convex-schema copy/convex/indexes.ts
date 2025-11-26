14dca040cf076c413525d343487a518e11700d1f
ayush99336 <ayushkaushikhere@gmail.com>
2025-11-26 15:12:34 +0530

    cleanup

--- uno-convex-schema copy/convex/indexes.ts
+++
@@ -1,6 +0,0 @@
-// Placeholder file; Convex does not require manual index declarations in a separate file.
-// Remove legacy imports from 'convex-dev/server'.
-
-export {}; 
-
-// Indexes are defined on tables inline using .index() in schema.ts.
